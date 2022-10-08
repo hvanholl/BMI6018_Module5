99098a5a1630bba55e8840a086199536c189b5d3
hvanholl <97499214+hvanholl@users.noreply.github.com>
2022-10-07 22:12:05 -0600

    Add files via upload

--- recursive_inner_plus_HV20220925.py
+++ recursive_inner_plus_HV20220925.py
@@ -2,18 +2,23 @@
 
 input_list_1 = [1,2,3,4,[5,6,7,[8,9]]]
 input_list_2 = [5,[9,2, 1, 3, [5,6,7,[3,1,[0,1], 5, [1,2]], 5, 0]]]
+input_list_3 = [[0,1,2,3,[0,1]],[0,1,2,3,[0,1]]]
 
-def recurs(input_r):                                                    # define recursive function
+def recurs(input_r):                                                    
     if all(isinstance(j, int) for j in input_r) == True:                # if all items in list are integers
         output = [i + 1 for i in input_r]                               # create output list with this sublist
-        return output                                                   # return the output
-    else:                                                               # otherwise
-        input_r = [i for i in input_r if isinstance(i, list) == True]   # create new input list with indexes that are lists
-        input_r = input_r[0]                                            # make the first index the new list.
-                                                                        # If this step isn't here it will be an infinite loop.
-        return recurs(input_r)                                          # call the function on itself and return        
+        return output                                                   
+    else:                                                               
+        input_temp = [i for i in input_r if isinstance(i, list) == True]   # create new input list with indexes that are lists
+        input_r = [int for i in input_temp for int in i]                   # flatten the list
+        return recurs(input_r)                                             # recursion until everything is a list 
 
 one = recurs(input_list_1)
 two = recurs(input_list_2)
+three = recurs(input_list_3)
+
 print(one)
 print(two)
+print(three)
+
+
--- while_inner_plus_HV20220925.py
+++ while_inner_plus_HV20220925.py
@@ -2,7 +2,8 @@
 #As an example:
 
 input_list_1 = [1,2,3,4,[5,6,7,[8,9]]]
-input_list_2 = [5,[9, 2, 1, 3, [5, 6, 7, [3, 1, [0, 7], 5]]]]
+input_list_2 = [5,[9,2, 1, 3, [5,6,7,[3,1,[0,1], 5, [1,2]], 5, 0]]]
+input_list_3 = [[0,1,2,3,[0,1]],[0,1,2,3,[0,1]]]
 
 def while_list(input_list):
     i = 0                                                 # Initialize loop                                
@@ -11,12 +12,16 @@ def while_list(input_list):
             output = [i + 1 for i in input_list]          # Create output list with that index
             return output                                 # Return value to function
         i += 1                                            # increase index by 1
-        if isinstance(input_list[i], list):               # If idex is a list itself
-            input_list = input_list[i]                    # Make the input list equal to that list
+        if any(isinstance(j, list) for j in input_list):               # If idex is a list itself
+            input_temp = [i for i in input_list if isinstance(i, list) == True] # Make the input list a list of indices that are lists
+            input_list = [int for i in input_temp for int in i]                   # flatten the list
             i = 0                                         # re-initialize loop to loop through nested list
 
 
-one = while_list(input_list_1)                # Call function and store in variable
+one = while_list(input_list_1)
 two = while_list(input_list_2)
-print(one)                                    # print
+three = while_list(input_list_3)
+
+print(one)
 print(two)
+print(three)
